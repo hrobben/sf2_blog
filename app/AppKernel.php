55defdd9435ee773192fc83d47dbbfff34ecddbc
henry <hrobben@robusoft.nl>
2015-07-04 10:23:40 +0200

    Dit is een blog-page uit een tutor symfony2 en gaan we gebruiken voor digieet

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -19,7 +19,7 @@ class AppKernel extends Kernel
             new Blogger\BlogBundle\BloggerBlogBundle(),
             new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle(),
             new Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle(),
-            new Ivory\CKEditorBundle\IvoryCKEditorBundle(),
+            new Stfalcon\Bundle\TinymceBundle\StfalconTinymceBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
