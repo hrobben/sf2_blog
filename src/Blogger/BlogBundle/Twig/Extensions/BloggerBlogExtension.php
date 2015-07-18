1f730896b041ddec8a9c8512d05721d7bf0860e7
henry <hrobben@robusoft.nl>
2015-07-18 21:26:01 +0200

    Dit is een blog-page uit een tutor symfony2 en gaan we gebruiken voor digieet

--- src/Blogger/BlogBundle/Twig/Extensions/BloggerBlogExtension.php
+++ src/Blogger/BlogBundle/Twig/Extensions/BloggerBlogExtension.php
@@ -16,25 +16,25 @@ class BloggerBlogExtension extends \Twig_Extension
     {
         $delta = time() - $dateTime->getTimestamp();
         if ($delta < 0)
-            throw new \InvalidArgumentException("createdAgo is unable to handle dates in the future");
+            throw new \InvalidArgumentException('createdAgo is unable to handle dates in the future');
 
         $duration = "";
         if ($delta < 60) {
             // Seconds
             $time = $delta;
-            $duration = $time . " second" . (($time > 1) ? "s" : "") . " ago";
+            $duration = $time . ' ' . (($time > 1) ? 'seconds' : 'second'). ' ago';
         } else if ($delta < 3600) {
             // Mins
             $time = floor($delta / 60);
-            $duration = $time . " minute" . (($time > 1) ? "s" : "") . " ago";
+            $duration = $time . ' ' . (($time > 1) ? 'minutes' : 'minute') . ' ago';
         } else if ($delta < 86400) {
             // Hours
             $time = floor($delta / 3600);
-            $duration = $time . " hour" . (($time > 1) ? "s" : "") . " ago";
+            $duration = $time . ' ' . (($time > 1) ? 'hours' : 'hour') . ' ago';
         } else {
             // Days
             $time = floor($delta / 86400);
-            $duration = $time . " day" . (($time > 1) ? "s" : "") . " ago";
+            $duration = $time . ' ' . (($time > 1) ? 'days' : 'day') . ' ago';
         }
 
         return $duration;
